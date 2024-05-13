3919bb676ff8e4bb7cd1f75395f0daadfce1a80b
Fr3kii <149470874+Fr3kii@users.noreply.github.com>
2024-05-13 11:51:52 +0200

    Update ft_atoi.c

--- libft/ft_atoi.c
+++ libft/ft_atoi.c
@@ -12,29 +12,24 @@
 
 #include "../inc/libft.h"
 
-int	ft_atoi(const char *nptr)
+long	ft_atoi(const char *nptr)
 {
-	unsigned long	i;
+	long	i;
 	int				sign;
 
 	i = 0;
 	sign = 1;
 	while (*nptr != '\0' && (*nptr == 32 || (*nptr >= 9 && *nptr <= 13)))
 		nptr++;
-	if (*nptr == 45)
+	if (*nptr == 45 || *nptr == 43)
 	{
-		sign *= -1;
+		if (*nptr == 45)
+			sign = -1;
 		nptr++;
 	}
-	else if (*nptr == 43)
-		nptr++;
 	while (ft_isdigit(*nptr) && *nptr != '\0')
 	{
 		i = i * 10 + (*nptr - '0');
-		if ((i > LONG_MAX) && sign == 1)
-			return (-1);
-		else if ((i > LONG_MAX) && sign == -1)
-			return (0);
 		nptr++;
 	}
 	return (i * sign);
